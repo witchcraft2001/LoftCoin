3ea1a943327260fab71098f0ef9ca2e052cc0061
DmitryM <mikhaltchenkov@gmail.com>
2022-06-05 18:40:29 +0300

    added navigation

--- app/src/main/java/com/dmdev/loftcoin/ui/main/MainActivity.kt
+++ app/src/main/java/com/dmdev/loftcoin/ui/main/MainActivity.kt
@@ -2,11 +2,28 @@ package com.dmdev.loftcoin.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.dmdev.loftcoin.R
+import com.dmdev.loftcoin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //        findNavController()
+        with(binding) {
+//            bottomNavigationView.setupWithNavController(
+//                findNavController(R.id.fragmentContainerView)
+//            )
+            bottomNavigationView.setupWithNavController(fragmentContainerView.findNavController())
+        }
     }
 }
