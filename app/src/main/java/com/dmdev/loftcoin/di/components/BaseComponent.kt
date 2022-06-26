1190c37255ba0d52bcd025a90cad527d177e7f06
DmitryM <mikhaltchenkov@gmail.com>
2022-06-26 08:41:20 +0300

    added currency repo

--- app/src/main/java/com/dmdev/loftcoin/di/components/BaseComponent.kt
+++ app/src/main/java/com/dmdev/loftcoin/di/components/BaseComponent.kt
@@ -2,8 +2,10 @@ package com.dmdev.loftcoin.di.components
 
 import android.content.Context
 import com.dmdev.loftcoin.data.repository.CoinsRepo
+import com.dmdev.loftcoin.data.repository.CurrencyRepo
 
 interface BaseComponent {
     fun context() : Context
     fun coinsRepo() : CoinsRepo
+    fun currencyRepo() : CurrencyRepo
 }
